0fcb1cf729999815d2a6473cbdd69e4c8d1722f4
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-05-29 17:02:22 -0300

    added: DB.js

---
+++ index.js
@@ -0,0 +1,10 @@
+global.PROD = ((process.env.NODE_ENV || "").toLowerCase() === 'prod') ? true : false;
+global.ROOT = __dirname;
+
+const app = require(`./config/custom-express`)();
+const port = (PROD) ? 3000 : 3333;
+
+app.listen(port, async () => {
+    console.clear();
+    console.log(`Rodando na porta ${port}`);
+});
